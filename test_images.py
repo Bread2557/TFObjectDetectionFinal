435de6424e1727e45eeb78e70823c4dc314a9c8a
Louis <LouisDC@gmx.de>
2023-01-09 20:41:22 +0100

    test_images.py korrigiert

--- test_images.py
+++ test_images.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 import imghdr
 
-data_dir = "C:\\Projekte\\Python\\P310TFODTest\\Tensorflow\\workspace\\images\\test"
+data_dir = "C:\\Projekte\\Python\\P310TFODTest\\Tensorflow\\workspace\\images\\train"
 image_extensions = [".png", ".jpg", ".jpeg"]  # add there all your images file extensions
 
 img_type_accepted_by_tf = ["bmp", "gif", "jpeg", "png"]
 for filepath in Path(data_dir).rglob("*"):
     if filepath.suffix == ".xml":
         image_path = filepath.parent / filepath.stem
-        if not any(image_path.with_suffix(ext).exists() for ext in image_extensions):
+        if not any(filepath.with_suffix(ext).exists() for ext in image_extensions):
             print("No image found for xml file: " + str(filepath))
     elif filepath.suffix in image_extensions:
         xml_path = filepath.parent / (filepath.stem + ".xml")
